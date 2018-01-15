f521e08368ed65639b86bd0f74a11ae92246d15e
Seth Borne <sethborne@gmail.com>
2018-01-15 06:16:25 -0800

    Recursive Thinking Website - Home Screen - Create HTML CSS JS Files

---
+++ templates/homeScreen.js
@@ -0,0 +1,37 @@
+// console.log("Loaded at HomeScreen");
+
+// capture header signup button
+const btnSignUpHead = document.getElementById('btnSignUp');
+// console.log(btnSignUpHead);
+// caputer header login button
+const btnLoginHead = document.getElementById('btnLogin');
+// console.log(btnLoginHead);
+
+// capture modal Signup
+const modalContSignup = document.getElementById('modalSignUp');
+// console.log(modalContSignup);
+
+// capture modal Container Login
+const modalContLogin = document.getElementById('modalLogin');
+// console.log(modalContLogin);
+
+btnSignUpHead.onclick = function() {
+    modalContSignup.style.display = "block";
+}
+
+btnLoginHead.onclick = function() {
+    modalContLogin.style.display = "block";
+}
+
+// if the user clicks outside either modal, close that modal
+window.onclick = function(event) {
+    if (event.target == modalContSignup) {
+        modalContSignup.style.display = "none";
+    }
+    else if(event.target == modalContLogin){
+        modalContLogin.style.display = "none";
+    }
+    else{
+        console.log(event.target.parentElement.style.display);
+    }
+}
