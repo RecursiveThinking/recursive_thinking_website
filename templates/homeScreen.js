d7ffaa0f537f8e2b7e21942d91a27386edfdc2cc
Seth Borne <sethborne@gmail.com>
2018-01-29 11:56:39 -0800

    RTW - homeScreen html/css/js - Connect Body SignUp Button to Modal

--- templates/homeScreen.js
+++ templates/homeScreen.js
@@ -1,14 +1,16 @@
 // console.log("Loaded at HomeScreen");
 
 // capture header signup button
-const btnSignUpHead = document.getElementById('btnSignUp');
+const btnSignUpHead = document.getElementById('btnSignUpHead');
 // console.log(btnSignUpHead);
 // caputer header login button
-const btnLoginHead = document.getElementById('btnLogin');
+const btnLoginHead = document.getElementById('btnLoginHead');
 // console.log(btnLoginHead);
+const btnSignUpBody = document.getElementById('btnSignUpBody')
+console.log(btnSignUpBody);
 
 // capture modal Signup
-const modalContSignup = document.getElementById('modalSignUp');
+const modalContSignUp = document.getElementById('modalSignUp');
 // console.log(modalContSignup);
 
 // capture modal Container Login
@@ -16,17 +18,21 @@ const modalContLogin = document.getElementById('modalLogin');
 // console.log(modalContLogin);
 
 btnSignUpHead.onclick = function() {
-    modalContSignup.style.display = "block";
+    modalContSignUp.style.display = "block";
 }
 
 btnLoginHead.onclick = function() {
     modalContLogin.style.display = "block";
 }
 
+btnSignUpBody.onclick = function(){
+    modalContSignUp.style.display = "block";
+}
+
 // if the user clicks outside either modal, close that modal
 window.onclick = function(event) {
-    if (event.target == modalContSignup) {
-        modalContSignup.style.display = "none";
+    if (event.target == modalContSignUp) {
+        modalContSignUp.style.display = "none";
     }
     else if(event.target == modalContLogin){
         modalContLogin.style.display = "none";
