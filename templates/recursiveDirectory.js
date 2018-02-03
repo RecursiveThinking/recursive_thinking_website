785ce4d9a1df142c4e9f2ff842349925672a09f6
Seth Borne <sethborne@gmail.com>
2018-02-02 21:42:15 -0800

    RTW - recursiveDirectory js - updates

---
+++ templates/recursiveDirectory.js
@@ -0,0 +1,15 @@
+
+
+function logSomething(){
+    console.log('TRIAL');
+}
+
+logSomething();
+
+console.log("Recursive Directory");
+
+const allUsersDirectory = JSON.parse(localStorage.getItem('allUsers'));
+console.log(allUsersDirectory);
+
+const gridContainer = document.getElementById('recursiveDirectoryHTML');
+// console.log(gridContainer.#document);
