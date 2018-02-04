deedfcc92b0a62abe0077de21022b646d2f54f84
Seth Borne <sethborne@gmail.com>
2018-02-03 23:13:38 -0800

    RTW - recursiveDirectory html/js - Load AllUsers and AllLessons

--- templates/recursiveDirectory.js
+++ templates/recursiveDirectory.js
@@ -1,15 +1,27 @@
+var allUsers = JSON.parse(localStorage.getItem('allUsers'));
+var allLessons = JSON.parse(localStorage.getItem('allLessons'));
 
+function buildRecursiveDirectory(){
+    const gridContainer = document.getElementById('gridContainer');
+    console.log(gridContainer);
+    for(let i = 0; i < allUsers.length; i += 1){
+        // console.log(`${allUsers[i].name} | ${allUsers[i].title} | ${allUsers[i].location}`);
+        const div = document.createElement('div');
+        // add class
+        div.className = `cell`;
+        // article
+        const articleCell = document.createElement('article');
+        // add art class
+        articleCell.className = `directory-card`
+            // add article image
+            const articleImage = document.createElement('img');
+            // add img class
+            articleImage.className = `devPicture`
+        div.appendChild(articleCell);
+        
+        
+        gridContainer.appendChild(div)
+    }
+    console.log(allUsers);
 
-function logSomething(){
-    console.log('TRIAL');
 }
-
-logSomething();
-
-console.log("Recursive Directory");
-
-const allUsersDirectory = JSON.parse(localStorage.getItem('allUsers'));
-console.log(allUsersDirectory);
-
-const gridContainer = document.getElementById('recursiveDirectoryHTML');
-// console.log(gridContainer.#document);
