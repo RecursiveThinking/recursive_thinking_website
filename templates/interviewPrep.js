a01574e91c89ad1160c8a4138e9dcf046c9dd742
Seth Borne <sethborne@gmail.com>
2018-02-03 23:12:31 -0800

    RTW - interviewPrep js - Error Fix

--- templates/interviewPrep.js
+++ templates/interviewPrep.js
@@ -1,38 +1,38 @@
-const btnSidebarInterviewPrep = document.getElementById('btnSidebarInterviewPrep');
+// const btnSidebarInterviewPrep = document.getElementById('btnSidebarInterviewPrep');
 // console.log(btnSidebarVoteLessons);
 
-btnSidebarInterviewPrep.addEventListener('click', function(){
-    const btnSubmitInterviewQuestion = document.getElementById('btnSubmitInterviewQuestion');
-    console.log(btnSubmitInterviewQuestion);
-    // }, false);
+// btnSidebarInterviewPrep.addEventListener('click', function(){
+//     const btnSubmitInterviewQuestion = document.getElementById('btnSubmitInterviewQuestion');
+//     console.log(btnSubmitInterviewQuestion);
+//     // }, false);
     
-    const btnAnswerQuestion = document.getElementById('btnAnswerQuestion');
-    console.log(btnAnswerQuestion);
+//     const btnAnswerQuestion = document.getElementById('btnAnswerQuestion');
+//     console.log(btnAnswerQuestion);
 
-    // modal
-    const modalContSubmitInterviewQuestion = document.getElementById('modalSubmitInterviewQuestion');
+//     // modal
+//     const modalContSubmitInterviewQuestion = document.getElementById('modalSubmitInterviewQuestion');
     
-    const modalConstAnswerInterviewQuestion = document.getElementById('modalAnswerInterviewQuestion');
+//     const modalConstAnswerInterviewQuestion = document.getElementById('modalAnswerInterviewQuestion');
     
     
-    btnSubmitInterviewQuestion.onclick = function() {
-        modalContSubmitInterviewQuestion.style.display = "block";
-    }
+//     btnSubmitInterviewQuestion.onclick = function() {
+//         modalContSubmitInterviewQuestion.style.display = "block";
+//     }
     
-    btnAnswerQuestion.onclick = function() {
-        modalConstAnswerInterviewQuestion.style.display = "block";
-    }
+//     btnAnswerQuestion.onclick = function() {
+//         modalConstAnswerInterviewQuestion.style.display = "block";
+//     }
     
-    window.onclick = function(event) {
-        if (event.target == modalContSubmitInterviewQuestion) {
-            modalContSubmitInterviewQuestion.style.display = "none";
-        }
-        else if (event.target == modalConstAnswerInterviewQuestion) {
-            modalConstAnswerInterviewQuestion.style.display = "none";
-        }
-        else{
-            // console.log(event.target.parentElement.style.display);
-            console.log("Clicking on Something Not a Modal");
-        }
-    }
-});
+//     window.onclick = function(event) {
+//         if (event.target == modalContSubmitInterviewQuestion) {
+//             modalContSubmitInterviewQuestion.style.display = "none";
+//         }
+//         else if (event.target == modalConstAnswerInterviewQuestion) {
+//             modalConstAnswerInterviewQuestion.style.display = "none";
+//         }
+//         else{
+//             // console.log(event.target.parentElement.style.display);
+//             console.log("Clicking on Something Not a Modal");
+//         }
+//     }
+// });
