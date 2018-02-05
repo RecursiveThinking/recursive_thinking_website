f0194473af0152ebe116b0fb038d37c43d736ff4
Seth Borne <sethborne@gmail.com>
2018-02-05 15:07:15 -0800

    RTW - upcomingLessons html/js - Generate Content from JS and Lesson Obj

---
+++ templates/upcomingLessons.js
@@ -0,0 +1,15 @@
+var allUsers = JSON.parse(localStorage.getItem('allUsers'));
+var allLessons = JSON.parse(localStorage.getItem('allLessons'));
+var allProfileStatistics = JSON.parse(localStorage.getItem('allProfileStatistics'));
+
+function buildUpComingLessons(){
+    // grab lesson card
+    const upComingLessonsCard = document.getElementById('upComingLessonsCard');
+    console.log(upComingLessonsCard);
+    
+    if(allProfileStatistics.length > 0){
+        
+    } else {
+        
+    }
+}
