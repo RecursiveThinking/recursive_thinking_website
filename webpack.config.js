05c32d2376903e73ac856b277d708224882aa6d1
Austin Nevins <amnevins@gmail.com>
2018-02-18 13:36:57 -0800

    Fully create build directory on npm run webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: './main.js',
+    plugins: [
+        new CleanWebpackPlugin(['build']),
+        new CopyWebpackPlugin([
+            { from: './layout.html', to: './', force: true },
+            { from: './main.css', to: './', force: true },
+            { from: './css/', to: './css', force: true },
+            { from: './public/', to: './public', force: true },
+            { from: './templates/', to: './templates', force: true }
+        ])
+    ],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'main.bundle.js'
