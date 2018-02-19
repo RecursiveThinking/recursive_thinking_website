3d0099bb9ee79b2cf3b07cc4dd4eb5ad4027fcc9
Austin Nevins <amnevins@gmail.com>
2018-02-18 23:49:43 -0800

    Added webpack hotreloading and AWS Codestar integration for backend foundation

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,36 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const bucketPath = "https://s3-us-west-2.amazonaws.com/aws-codestar-us-west-2-131603044023-devdaywebsite-app/";
 
 module.exports = {
     entry: './main.js',
     plugins: [
         new CleanWebpackPlugin(['build']),
+        new HtmlWebpackPlugin({
+            template: './layout.html',
+            filename: './index.html'
+        }),
         new CopyWebpackPlugin([
-            { from: './layout.html', to: './', force: true },
-            { from: './main.css', to: './', force: true },
-            { from: './css/', to: './css', force: true },
             { from: './public/', to: './public', force: true },
-            { from: './templates/', to: './templates', force: true }
+            { from: './templates/', to: './templates', force: true },
+            { from: './css/', to: './css', force: true },
+            { from: './buildspec.yml', to: './', force: true },
+            { from: './template.yml', to: './', force: true },
+            { from: './testlambda.js', to: './', force: true },
+            { from: './main.css', to: './', force: true }
         ])
     ],
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'main.bundle.js'
+        filename: './public/main.bundle.js'
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
     },
     module: {
         loaders: [
