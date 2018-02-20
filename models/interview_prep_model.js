8a6e2c8433bf213d612605ddb22925d53c82db6f
Seth Borne <sethborne@gmail.com>
2018-02-19 22:32:58 -0800

    RTW - interviewPrep css/js/html/modeljs - this commit fixes #7 + #13 - still needs click action to display answers

--- models/interview_prep_model.js
+++ models/interview_prep_model.js
@@ -1,18 +1,38 @@
-import { utils } from '../scripts/global';
+import { utils, data } from '../scripts/global';
 import { templates, fill } from '../scripts/templater';
 
 export const getInterviewPrepModel = () => {
     
     // #TODO: API Call goes here
-    const allInterviewQuestions = JSON.parse(localStorage.getItem('allInterviewQuestions'));
+    // const allInterviewQuestions = JSON.parse(localStorage.getItem('allInterviewQuestions'));
+    const allInterviewQuestions = data.getAllInterviewQuestions();
+    console.log(allInterviewQuestions);
+    const allAnswersToQuestions = data.getAllAnswersToQuestions();
+    console.log(allAnswersToQuestions);
+    const currentUser = data.getCurrentUser();
+    console.log("currentUser at InterviewPrep", currentUser);
     
     return {
         interviewPrep: allInterviewQuestions.map(question => {
-            return fill(templates.interviewPrep.interviewQuestion, {
-                // day: formattedDate.dateOfMonth,
-                // month: formattedDate.monthAsString,
-                // year: formattedDate.year,
-                // title: lesson.title
+            const formattedDate = utils.getFormattedDate(new Date(question.submitted));
+            
+            return fill(templates.interviewPrep.tempIdInterviewQuestion, {
+                title: `${question.title}:`,
+                date: formattedDate.upComingDateStringAmericanNaming,
+                description: question.description,
+                questionAnswersCount: utils.getCountString('question', question),
+                questionAnswers: question.answersToQuestion.map(answerId => {
+                    let getAnswer = allAnswersToQuestions.filter((answerObj) => {
+                        return answerObj["_id"] === answerId;
+                    });
+                    getAnswer = getAnswer[0];
+                    return fill(templates.interviewPrep.tempIdInterviewQuestionAnswer, {
+                        
+                        submitted: utils.getFormattedDate(new Date(getAnswer.submitted)).upComingDateStringAmericanWithSlash,
+                        description: getAnswer.description,
+                    })
+                }
+                )
             })
         })
     }
--- templates/interviewPrep.js
+++ templates/interviewPrep.js
@@ -2,32 +2,32 @@ export const modalInterview = () => {
 
     const btnSubmitInterviewQuestion = document.getElementById('btnSubmitInterviewQuestion');
     
-    const btnAnswerQuestion = document.getElementById('btnAnswerQuestion');
+    // const btnAnswerQuestion = document.getElementById('btnAnswerQuestion');
 
 //     // modal
     const modalContSubmitInterviewQuestion = document.getElementById('modalSubmitInterviewQuestion');
     
-    const modalConstAnswerInterviewQuestion = document.getElementById('modalAnswerInterviewQuestion');
+    // const modalConstAnswerInterviewQuestion = document.getElementById('modalAnswerInterviewQuestion');
     
     
     btnSubmitInterviewQuestion.onclick = function() {
         modalContSubmitInterviewQuestion.style.display = "block";
     }
     
-    btnAnswerQuestion.onclick = function() {
-        modalConstAnswerInterviewQuestion.style.display = "block";
-    }
+    // btnAnswerQuestion.onclick = function() {
+    //     modalConstAnswerInterviewQuestion.style.display = "block";
+    // }
     
     window.onclick = function(event) {
         if (event.target == modalContSubmitInterviewQuestion) {
             modalContSubmitInterviewQuestion.style.display = "none";
         }
-        else if (event.target == modalConstAnswerInterviewQuestion) {
-            modalConstAnswerInterviewQuestion.style.display = "none";
-        }
+        // else if (event.target == modalConstAnswerInterviewQuestion) {
+        //     modalConstAnswerInterviewQuestion.style.display = "none";
+        // }
         else{
             // console.log(event.target.parentElement.style.display);
-            console.log("Clicking on Something Not a Modal");
+            console.log("Clicking on Something Not a Modal - Interview");
         }
     }
 }
