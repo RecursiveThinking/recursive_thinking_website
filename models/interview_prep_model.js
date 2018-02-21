f37a1f53c9315bf5cdff2a84137c91fcedd71cc4
Seth Borne <sethborne@gmail.com>
2018-02-21 15:47:21 -0800

    RTW - interviewPrep js html - this commit fixes #38 - dynamic loading of categories

--- models/interview_prep_model.js
+++ models/interview_prep_model.js
@@ -11,15 +11,21 @@ export const getInterviewPrepModel = () => {
     console.log(allAnswersToQuestions);
     const currentUser = data.getCurrentUser();
     console.log("currentUser at InterviewPrep", currentUser);
-    
     return {
         interviewPrep: allInterviewQuestions.map(question => {
+            let questionCategoryArray = utils.returnArrayOfApplicableItems('allCategories', question.categories);
+            console.log(questionCategoryArray);
             const formattedDate = utils.getFormattedDate(new Date(question.submitted));
             
             return fill(templates.interviewPrep.tempIdInterviewQuestion, {
                 title: `${question.title}:`,
                 date: formattedDate.upComingDateStringAmericanNaming,
                 description: question.description,
+                questionCategories: questionCategoryArray.map((category) => {
+                    return fill(templates.interviewPrep.tempIdInterviewQuestionCategory, {
+                        category: category
+                    })
+                }),
                 questionAnswersCount: utils.getCountString('question', question),
                 questionAnswers: question.answersToQuestion.map(answerId => {
                     let getAnswer = allAnswersToQuestions.filter((answerObj) => {
