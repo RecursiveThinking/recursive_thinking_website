c77702f6d71373f458e7974aa449c910ea6a0816
clayton-perszyk <clayperszyk@gmail.com>
2018-02-20 18:48:07 -0800

    replace the image in profile

--- templates/editProfile.js
+++ templates/editProfile.js
@@ -1,8 +1,19 @@
 
 export const editProfile = () => {
   const editProfilePicButton = document.getElementById("profile-picture");
+  const profilePicture = document.getElementsByClassName("avatarPicXL")[0];
+  const renderImage = (file) => {
+    const reader = new FileReader();
 
-  editProfilePicButton.addEventListener('click', function(event){
+    reader.onload = function(event) {
+      const imageUrl = event.target.result;
+      profilePicture.setAttribute('src', imageUrl);
+    }
 
-  });
+    reader.readAsDataURL(file);
+  }
+
+  document.getElementById('file').addEventListener('change', function(event) {
+     renderImage(this.files[0]);
+  }, false);
 }
