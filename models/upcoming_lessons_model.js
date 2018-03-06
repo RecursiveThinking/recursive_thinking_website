055ab39a2c628e168d7151811a97c2ce41905809
Seth Borne <sethborne@gmail.com>
2018-03-05 21:21:08 -0800

    RTW - upComingLessons html css - this commit fixes #62 and adds dynamic stars

--- models/upcoming_lessons_model.js
+++ models/upcoming_lessons_model.js
@@ -1,11 +1,11 @@
-import { utils } from '../scripts/global';
+import { utils, data } from '../scripts/global';
 import { templates, fill } from '../scripts/templater';
 
 export const getUpcomingLessonsModel = () => {
     
     // #TODO: API Call goes here
     const allLessons = JSON.parse(localStorage.getItem('allLessons'));
-    
+    const currentUser = data.getCurrentUser();
     return {
         upcomingLessons: allLessons.map(lesson => {
             let formattedDate = utils.getFormattedDate(new Date(lesson.date));
@@ -13,6 +13,8 @@ export const getUpcomingLessonsModel = () => {
                 day: formattedDate.dateOfMonth,
                 month: formattedDate.monthAsString,
                 year: formattedDate.year,
+                // this function
+                isStar: utils.getBoolIfUserAttending(lesson._id, currentUser.lessonsAttending),
                 title: lesson.title
             })
         })
