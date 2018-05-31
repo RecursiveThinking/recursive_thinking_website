98219e5c3b8f0c76912da76bc668abb326784470
Seth Borne <sethborne@gmail.com>
2018-05-31 15:16:27 -0700

    changes Amplify configuration for new user pool in template

--- main.js
+++ main.js
@@ -28,13 +28,44 @@ import './public/images/avatar6.png'
 
 Amplify.configure({
     Auth: {
-        // REQUIRED - Amazon Cognito Identity Pool ID
-        identityPoolId: 'us-west-2:35b0c13e-47e0-41e4-b5c8-e3924d86d107',
+        // THIS IS ACTUALLY OPTIONAL (OR MAYBE REQUIRED IF IT ACTUALLY IS CONFIGURED CORRECTLY) - Amazon Cognito Identity Pool ID -- as of 2018 05 31 this does nothing...not configured
+        // identityPoolId: 'us-west-2:35b0c13e-47e0-41e4-b5c8-e3924d86d107',
         // REQUIRED - Amazon Cognito Region
         region: 'us-west-2',
-        // OPTIONAL - Amazon Cognito User Pool ID
-        userPoolId: 'us-west-2_570GEyJgs',
-        // OPTIONAL - Amazon Cognito Web Client ID
-        userPoolWebClientId: '1tt5sh3cf3csicaqcpg0a4jiva'
+        // THIS IS REQUIRED - Amazon Cognito User Pool ID
+        // \/-- below is for old user pool - recursivethinking
+        // userPoolId: 'us-west-2_570GEyJgs',
+        // \/-- this if from new user pool
+        userPoolId: 'us-west-2_wlW7N1Kcp',
+        // THIS IS REQUIRED - Amazon Cognito Web Client ID
+        // \/-- below is for old user pool - recursivethinking
+        // userPoolWebClientId: '1tt5sh3cf3csicaqcpg0a4jiva'
+        // \/-- this if from new user pool
+        userPoolWebClientId: '57te4kfc511ietb5dpe3tkkm3e'
     }
 });
+
+// OLD
+////////
+// Pool Id us-west-2_570GEyJgs
+// Pool ARN arn:aws:cognito-idp:us-west-2:131603044023:userpool/us-west-2_570GEyJgs
+
+// App Clients
+// recursivethinkingwebsite
+// App Client Id:  1tt5sh3cf3csicaqcpg0a4jiva
+
+
+// NEW
+
+// Pool Id us-west-2_wlW7N1Kcp
+// Pool ARN arn:aws:cognito-idp:us-west-2:131603044023:userpool/us-west-2_wlW7N1Kcp
+
+// App Clients
+// RTW
+// App Client Id:  57te4kfc511ietb5dpe3tkkm3e
+
+// Federated Entities - recursivethinkingindentity - edit - 
+
+//  category authentication providers - cognito tab
+
+// changed user pool id from us-west-2_570GEyJgs -to- us-west-2_wlW7N1Kcp
