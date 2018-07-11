f8b5417a0a537618be08d39f6aac845609d405da
Seth Borne <sethborne@gmail.com>
2018-07-11 10:18:15 -0700

    moves s3UploadSecrets js to credentials folder

---
+++ credentials/s3UploadSecrets.js
@@ -0,0 +1,5 @@
+import s3UploadSecrets from '../secrets/s3UploadSecrets.json'
+console.log(s3UploadSecrets);
+
+export const s3UploadCreds = s3UploadSecrets
+console.log('s3UploadCreds', s3UploadCreds);
