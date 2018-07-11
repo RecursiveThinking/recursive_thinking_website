bbb3bd71b1c142111358a356e43ae65d23e93d96
Seth Borne <sethborne@gmail.com>
2018-07-11 10:22:21 -0700

    updates cognitoCreds js removes old code

--- credentials/cognitoCreds.js
+++ credentials/cognitoCreds.js
@@ -3,14 +3,3 @@ console.log(creds);
 
 export const credentials = creds
 console.log(credentials);
-
-// OLD credentials constructor
-
-// export const credentials = {
-//     region: "us-west-2",
-//     userPoolId: "us-west-2_LeVFVPfxn",
-//     userPoolWebClientId: "3art487ptkct3cjuppm2u7bla5",
-//     apiUrl: "https://ctrkbe79gj.execute-api.us-west-2.amazonaws.com/Prod"
-// }
-
-// console.log('credentials', credentials);
