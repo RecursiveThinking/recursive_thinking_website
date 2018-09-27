31b0f132536fd7fe51bcdf92c51a972c330c206d
Seth Borne <sethborne@gmail.com>
2018-09-26 22:36:43 -0700

    creats a generic modal component

--- src/components/common/modal/modal.js
+++ src/components/common/modal/modal.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const Modal = ({ handleClose, show, children }) => {
-  const showHideClassName = show ? "modal display-block" : "modal display-none";
-  
+const modal = (props) => {
+  // console.log(props)
+  const showHideClassName = props.show ? "modal display-block" : "modal display-none"
   return (
     <div className={showHideClassName}>
-      <section className="modal-main">
-        {children}
-        <button onClick={handleClose}>close</button>
+      <section className="modalCard">
+        <div onClick={props.handleClose} className="modalClose"><i className="fs33 fw300 fa fa-times"></i></div>
+        {props.content}
       </section>
     </div>
-  );
-};
+  )
+}
 
-export default Modal
+export default modal;
