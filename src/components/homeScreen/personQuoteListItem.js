b7c68e0eb79c4b02f7e0264c341c217218bfcfa7
Seth Borne <sethborne@gmail.com>
2018-09-26 22:35:31 -0700

    fixes key id error on person Quote List

--- src/components/homeScreen/personQuoteListItem.js
+++ src/components/homeScreen/personQuoteListItem.js
@@ -4,14 +4,14 @@ const personQuoteListItem = (obj) => {
   let imagePath = `../../../public/images/${obj.user.image}`;
   let alt = `A Photo of ${obj.user.name}`;
   return (
-    <li className="contentPersonQuote">
+    // <li className="contentPersonQuote">
       <section className="fc--disp-flex fc--fdir-col fc--aItem-ce ta-cent">
       {/* avatarBS */}
         <img className="avatarXL" src={imagePath} alt={alt} />
         <h3 className="fs36 fw500 mt40">{obj.user.title}</h3>
         <p className="fs24 fw300 mt15">{obj.user.quote}</p>
       </section>
-    </li>
+    // </li>
   )
 }
 
