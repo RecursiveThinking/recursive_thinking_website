1f5f2156467c7160550756e2b02d79d3d345312f
Seth Borne <sethborne@gmail.com>
2018-09-27 23:17:56 -0700

    adds lesson icons

--- src/components/unscheduledLessons/unscheduledLessonsListItem.js
+++ src/components/unscheduledLessons/unscheduledLessonsListItem.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Users = require('!json-loader!../../../data_returns/RecursiveThinkingDeveloperProfiles.json')
 
 import LessonMethods from '../../functions/lessonMethods'
+import UtilityMethods from '../../functions/utilityMethods'
+
 import LessonTaughtByThumbList from '../common/lesson/lessonTaughtByThumbList'
 import CurrentUserVotedOnLessonButton from './currentUserVotedOnLessonButton'
 
@@ -14,9 +16,21 @@ const unscheduledLessonListItem = ({currentUser, lesson}) => {
   // this uses includes array method to return true or false
   let boolHasCurrentUserVotedOnLesson = LessonMethods.hasCurrentUserVotedOnLesson(lesson, currentUser['userId']);
   // console.log(boolHasCurrentUserVotedOnLesson)
+  let optionList = UtilityMethods.generateOptionsList(currentUser.userId, currentUser.admin, lesson._lessonCreatedBy, 'fs28')
   return (
     <article className="card fc--disp-flex fc--fdir-col">
-      <h4 className="fs28 fw500 ls14 fcBlack">{lesson['title']}</h4>
+      <div className="grid grid--3of4">
+        <div className="grid-cell">
+          <h4 className="fs28 fw500 ls14 fcBlack">{lesson['title']}</h4>
+        </div>
+        <div className="grid-cell">
+          <div className="fc--disp-flex fc--fdir-row fc--jCont-fe fc--aItem-ce">
+            <div className="listOptions">
+              {optionList}
+            </div>
+          </div>
+        </div>
+      </div>
       <hr className="mt20" />
       <p className="fc-lessonText fs16 fw300 ls10 fcBlack mt10">{lesson['description']}</p>
       <hr className="mt10" />
