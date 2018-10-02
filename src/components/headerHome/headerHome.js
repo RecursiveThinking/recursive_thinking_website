49b5f953ca8c64167a49d919bdaee5a2db2b9a2c
Seth Borne <sethborne@gmail.com>
2018-10-01 23:37:03 -0700

    updates headerHome with appropriate comp state for modals

--- src/components/headerHome/headerHome.js
+++ src/components/headerHome/headerHome.js
@@ -7,26 +7,24 @@ class HeaderHome extends Component {
   constructor(props){
     super(props);
     this.state = {
-      showSignUp: false,
-      showSignIn: false,
+      showModalSignUp: false,
+      showModalSignIn: false,
     }
   }
   
-  showModalSignUp = () => {
-    this.setState({ showSignUp: true });
-  };
+  // showModalSignUp = () => {
+  //   this.setState({ showSignUp: true });
+  // };
 
-  hideModalSignUp = () => {
-    this.setState({ showSignUp: false });
-  };
-  
-  showModalSignIn = () => {
-    this.setState({ showSignIn: true });
-  };
-
-  hideModalSignIn = () => {
-    this.setState({ showSignIn: false });
-  };
+  // showModalSignIn = () => {
+  //   this.setState({ showSignIn: true });
+  // };
+  handleToggleModalSignUp(){
+    this.setState( {showModalSignUp: !this.state.showModalSignUp} )
+  }
+  handleToggleModalSignIn(){
+    this.setState( {showModalSignIn: !this.state.showModalSignIn} )
+  }
   
   render(){
     // console.log('MC Sign Up', ModalContent.signUp)
@@ -45,18 +43,24 @@ class HeaderHome extends Component {
           <div className="grid-cell fc--disp-flex fc--aItem-ce">
             <div className="fc-nav fc--disp-flex">
               <nav>
-                <button onClick={this.showModalSignUp} type="button" className="btn btnFillClrSchGreen00b371 signUp fs20 ls12">Sign Up</button>
-                  <Modal 
-                    content={<SignUpModalForm />}
-                    show={this.state.showSignUp}
-                    handleClose={this.hideModalSignUp}
-                  />
-                <button onClick={this.showModalSignIn} type="button" className="btn btnOutlineClrSchGreen00b371 signIn fs20 ls12">Sign In</button> 
-                  <Modal 
-                    content={<SignInModalForm />}
-                    show={this.state.showSignIn}
-                    handleClose={this.hideModalSignIn}
-                  />
+                <button onClick={() => this.handleToggleModalSignUp()} type="button" className="btn btnFillClrSchGreen00b371 signUp fs20 ls12">Sign Up</button>
+                  {
+                    this.state.showModalSignUp && 
+                    
+                    <Modal 
+                      onCloseRequest={() => this.handleToggleModalSignUp()}
+                      content={<SignUpModalForm />}
+                    />
+                  }
+                <button onClick={() => this.handleToggleModalSignIn()} type="button" className="btn btnOutlineClrSchGreen00b371 signIn fs20 ls12">Sign In</button> 
+                  {
+                    this.state.showModalSignIn &&
+                    
+                    <Modal
+                      onCloseRequest={() => this.handleToggleModalSignIn()}
+                      content={<SignInModalForm />}
+                    />
+                  }
               </nav>
             </div>
           </div>
