24559365264d27c2c208788dac81f917251bff28
Seth Borne <sethborne@gmail.com>
2018-10-01 23:36:31 -0700

    update generic modal component with appropriate comp state

--- src/components/common/modal/modal.js
+++ src/components/common/modal/modal.js
@@ -1,16 +1,66 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const modal = (props) => {
-  // console.log(props)
-  const showHideClassName = props.show ? "modal display-block" : "modal display-none"
-  return (
-    <div className={showHideClassName}>
-      <section className="modalCard">
-        <div onClick={props.handleClose} className="modalClose"><i className="fs33 fw300 fa fa-times"></i></div>
-        {props.content}
-      </section>
-    </div>
-  )
+import UtilityMethods from '../../../functions/utilityMethods'
+
+class Modal extends Component {
+  constructor(props){
+    super(props);
+    
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+  }
+  
+  componentDidMount(){
+    window.addEventListener('keyup', this.handleKeyUp, false);
+    document.addEventListener('click', this.handleOutsideClick, false);
+  }
+  
+  componentWillMount(){
+    window.removeEventListener('keyup', this.handleKeyUp, false);
+    document.removeEventListener('click', this.handleOutsideClick, false);
+  }
+  
+  handleKeyUp(event) {
+    const { onCloseRequest } = this.props;
+    const keys = {
+      27: () => {
+        event.preventDefault();
+        onCloseRequest();
+        window.removeEventListener('keyup', this.handleKeyUp, false);
+      },
+    };
+    
+    if (keys[event.keyCode]) { keys[event.keyCode](); }
+  }
+  
+  handleOutsideClick(event){
+    const { onCloseRequest } = this.props;
+    if(!UtilityMethods.isNil(this.modal)){
+      if(!this.modal.contains(event.target)){
+        onCloseRequest();
+        document.removeEventListener('click', this.handleOutsideClick, false);
+      }
+    }
+  }
+  
+  render(){
+    // console.log('props', this.props)
+    // console.log('content', this.props.content.type)
+    
+    return (
+      <div className="modal">
+        <section className="modalCard" ref={node => (this.modal = node)}>
+          <div className="modalClose">
+            <i 
+              onClick={this.props.onCloseRequest}
+              className="fs33 fw300 fa fa-times">
+            </i>
+          </div>
+          {this.props.content}
+        </section>
+      </div>
+    )
+  }
 }
 
-export default modal;
+export default Modal;
