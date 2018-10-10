772920cb5c0cac9cfaf1f6f8377fb016950cebcf
Seth Borne <sethborne@gmail.com>
2018-10-10 10:18:08 -0700

    profile Edit mock for routing/linking

---
+++ src/components/profile/edit/profileEdit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const editProfile = () => {
+  return (
+    <main className="content">
+      <div>Under Construction!</div>
+    </main>
+  )
+}
+
+export default editProfile;
