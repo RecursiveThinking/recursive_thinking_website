f6920fa533ad6eb83565968e193e195cb571e003
Seth Borne <sethborne@gmail.com>
2018-10-10 10:14:13 -0700

    destructuring props

--- src/components/adminPanel/adminPanelList.js
+++ src/components/adminPanel/adminPanelList.js
@@ -3,67 +3,90 @@ import React from 'react'
 import AdminPanelListItem from './adminPanelListItem'
 import DM from '../../standards/dictModel'
 
+export const TABLE_NAMES = {
+  users: 'users',
+  lessons: 'lessons',
+  interviewQuestions: 'interviewQuestions',
+  interviewQuestionsAnswers: 'interviewQuestionsAnswers'
+}
+
+
 const adminPanelList = ({...props}) => {
+  const { users, lessons, interviewQuestions, interviewQuestionsAnswers } = DM
   const tableTitle = [ 
     'User Admin Table',
     'Lesson Admin Table',
     'Interview Questions Admin Table',
     'Interview Questions Answers Admin Table'
   ]
+  const { 
+    user: { username, name, email, admin, inactive, isProfileSetup, lastActive},
+    lesson: { title, scheduled, lessonVotes, _lessonCreatedBy},
+    intQuestion,
+    intQuestionAnswer
+  } = DM
   const userTableHeadings = [
-    DM.user.name,
-    DM.user.username,
-    DM.user.email,
-    DM.user.admin,
-    DM.user.inactive,
-    DM.user.isProfileSetup,
-    DM.user.lastActive
+    username,
+    name,
+    email,
+    admin,
+    inactive,
+    isProfileSetup,
+    lastActive
   ]
   
   const lessonTableHeadings = [
-    DM.lesson.title,
-    DM.lesson.scheduled,
-    DM.lesson.lessonVotes,
-    DM.lesson._lessonCreatedBy,
+    title,
+    scheduled,
+    lessonVotes,
+    _lessonCreatedBy,
   ]
   
   const interviewQuestionsTableHeadings = [
-    DM.intQuestion.title,
-    DM.intQuestion._createdByUser
+    intQuestion.title,
+    intQuestion._createdByUser
   ]
   
   const interviewQuestionsAnswersTableHeadings = [
-    DM.intQuestionAnswer.title,
-    DM.intQuestionAnswer._createdByUser
+    intQuestionAnswer.title,
+    intQuestionAnswer._createdByUser
   ]
   
   return (
     <ul>
-      <AdminPanelListItem 
-        title={tableTitle[0]} 
-        table={'users'} 
-        tableHeadingArr={userTableHeadings} 
-        tableBodyArr={props.allUsers} 
-      />
-      <AdminPanelListItem 
-        title={tableTitle[1]} 
-        table={'lessons'} 
-        tableHeadingArr={lessonTableHeadings} 
-        tableBodyArr={props.allLessons}
-        lookupTableUsers={props.lookupTableUsers}
-      />
-      <AdminPanelListItem 
-        title={tableTitle[2]} 
-        table={'interviewQuestions'}
-        tableHeadingArr={interviewQuestionsTableHeadings} 
-        tableBodyArr={props.allInterviewQuestions}
-      />
-      <AdminPanelListItem
-        title={tableTitle[3]}
-        table={'interviewQuestionsAnswers'}
-        tableHeadingArr={interviewQuestionsAnswersTableHeadings} 
-        tableBodyArr={props.allInterviewQuestionsAnswers}
-      />
+      <li key={TABLE_NAMES[users]} className="grid grid--full">
+        <AdminPanelListItem 
+          title={tableTitle[0]} 
+          table={'users'} 
+          tableHeadingArr={userTableHeadings} 
+          tableBodyArr={props.allUsers} 
+        />
+      </li>
+      <li key={TABLE_NAMES[lessons]} className="grid grid--full">
+        <AdminPanelListItem 
+          title={tableTitle[1]} 
+          table={'lessons'} 
+          tableHeadingArr={lessonTableHeadings} 
+          tableBodyArr={props.allLessons}
+          lookupTableUsers={props.lookupTableUsers}
+        />
+      </li>
+      <li key={TABLE_NAMES[interviewQuestions]} className="grid grid--full">
+        <AdminPanelListItem 
+          title={tableTitle[2]} 
+          table={'interviewQuestions'}
+          tableHeadingArr={interviewQuestionsTableHeadings} 
+          tableBodyArr={props.allInterviewQuestions}
+        />
+      </li>
+      <li key={TABLE_NAMES[interviewQuestionsAnswers]} className="grid grid--full">
+        <AdminPanelListItem
+          title={tableTitle[3]}
+          table={'interviewQuestionsAnswers'}
+          tableHeadingArr={interviewQuestionsAnswersTableHeadings} 
+          tableBodyArr={props.allInterviewQuestionsAnswers}
+        />
+      </li>
     </ul>
     
 
