c36965ff1652d92d0315280b41cccaeafcee2eb1
Seth Borne <sethborne@gmail.com>
2018-10-10 10:20:48 -0700

    destructing props - standardize selectors - select lesson det attend list item

--- src/components/scheduledLessons/selectedLessonDetailAttendeesListItem.js
+++ src/components/scheduledLessons/selectedLessonDetailAttendeesListItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import DM from '../../standards/dictModel'
+
 const selectedLessonDetailAttendeesListItem = ({attendingUserObj}) => {
-  let avatarSrcPath = `/public/images/${attendingUserObj.avatar}`
-  let altString = `A Photo of ${attendingUserObj.name}`
+  const { user: { name, avatar}} = DM;
+  let avatarSrcPath = `/public/images/${attendingUserObj[avatar]}`
+  let altString = `A Photo of ${attendingUserObj[name]}`
   return (
     <div className="fc--disp-flex">
       <img className="avatarXXS fi--aSelf-ce" src={avatarSrcPath} alt={altString} />
-      <h5 className="fw300 ls14 fcBlack fi--aSelf-ce">{attendingUserObj.name}</h5>
+      <h5 className="fw300 ls14 fcBlack fi--aSelf-ce">{attendingUserObj[name]}</h5>
     </div>
   )
 }
