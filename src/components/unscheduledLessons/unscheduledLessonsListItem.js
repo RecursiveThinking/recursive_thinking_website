d663432a9dc42808dff0e7ee459d7c7d2faaa12d
Seth Borne <sethborne@gmail.com>
2018-10-10 10:21:06 -0700

    destructing props - stand selectors - unsched lesson list item

--- src/components/unscheduledLessons/unscheduledLessonsListItem.js
+++ src/components/unscheduledLessons/unscheduledLessonsListItem.js
@@ -8,20 +8,24 @@ import UtilityMethods from '../../functions/utilityMethods'
 import LessonTaughtByThumbList from '../common/lesson/lessonTaughtByThumbList'
 import CurrentUserVotedOnLessonButton from './currentUserVotedOnLessonButton'
 
+import DM from '../../standards/dictModel'
+
 const unscheduledLessonListItem = ({currentUser, lesson}) => {
-  // console.log('currUser', currentUser, currentUser['userId']);
-  let taughtByArray = lesson['lessonTaughtBy']
-  let taughtByUserArray = LessonMethods.getArrayofTaughtByUserObjs(taughtByArray, Users);
-  let voteCount = lesson.lessonVotes.length;
+  
+  const { lesson: { title, description, lessonTaughtBy, lessonVotes }, user: { userId, admin, _lessonCreatedBy }} = DM
+  
+  let taughtByUserArray = LessonMethods.getArrayofTaughtByUserObjs(lesson[lessonTaughtBy], Users);
+  let voteCount = lesson[lessonVotes].length;
   // this uses includes array method to return true or false
-  let boolHasCurrentUserVotedOnLesson = LessonMethods.hasCurrentUserVotedOnLesson(lesson, currentUser['userId']);
+  let boolHasCurrentUserVotedOnLesson = LessonMethods.hasCurrentUserVotedOnLesson(lesson, currentUser[userId]);
   // console.log(boolHasCurrentUserVotedOnLesson)
-  let optionList = UtilityMethods.generateOptionsList(currentUser.userId, currentUser.admin, lesson._lessonCreatedBy, 'fs28')
+  let optionList = UtilityMethods.generateOptionsList(currentUser[userId], currentUser[admin], lesson[_lessonCreatedBy], 'fs28')
+  
   return (
     <article className="card fc--disp-flex fc--fdir-col">
       <div className="grid grid--3of4">
         <div className="grid-cell">
-          <h4 className="fs28 fw500 ls14 fcBlack">{lesson['title']}</h4>
+          <h4 className="fs28 fw500 ls14 fcBlack">{lesson[title]}</h4>
         </div>
         <div className="grid-cell">
           <div className="fc--disp-flex fc--fdir-row fc--jCont-fe fc--aItem-ce">
@@ -32,7 +36,7 @@ const unscheduledLessonListItem = ({currentUser, lesson}) => {
         </div>
       </div>
       <hr className="mt20" />
-      <p className="fc-lessonText fs16 fw300 ls10 fcBlack mt10">{lesson['description']}</p>
+      <p className="fc-lessonText fs16 fw300 ls10 fcBlack mt10">{lesson[description]}</p>
       <hr className="mt10" />
       <div className="fc-taughtBy">
         <h5 className="fw300 ls14 ttup fcBlack mt10">Taught By:</h5>
