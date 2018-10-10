c1f5bfe92ba3e57286b3d8b11dd3fbc6c5eaeb92
Seth Borne <sethborne@gmail.com>
2018-10-10 10:15:16 -0700

    destructing props - dash profile stat list

--- src/components/dash/dashboardProfileStatList.js
+++ src/components/dash/dashboardProfileStatList.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import DashboardProfileStatListItem from './dashboardProfileStatListItem'
 
+import DM from '../../standards/dictModel'
+
 const dashboardProfileStatList = ({userStats}) => {
+  const { 
+    user: {
+      profileStatsVisits,
+      profileStatsViewsGithub,
+      profileStatsViewsCodePen,
+      profileStatsViewsPortfolio,
+      profileStatsViewsLinkedIn,
+      profileStatsViewsResume
+    }
+  } = DM;
   const stats1 = [
-    [userStats[0], 'Profile Visits', 'profileStatsVisits'],
-    [userStats[1], 'GitHub Views', 'profileStatsViewsGithub'],
-    [userStats[2], 'CodePen Views', 'profileStatsViewsCodePen']
+    [userStats[0], 'Profile Visits', profileStatsVisits],
+    [userStats[1], 'GitHub Views', profileStatsViewsGithub],
+    [userStats[2], 'CodePen Views', profileStatsViewsCodePen]
   ]
   const stats2 = [
-    [userStats[3], 'Portfolio Views', 'profileStatsViewsPortfolio'],
-    [userStats[4], 'LinkedIn Views', 'profileStatsViewsLinkedIn'],
-    [userStats[5], 'Resume Views', 'profileStatsViewsResume']
+    [userStats[3], 'Portfolio Views', profileStatsViewsPortfolio],
+    [userStats[4], 'LinkedIn Views', profileStatsViewsLinkedIn],
+    [userStats[5], 'Resume Views', profileStatsViewsResume]
   ]
   
-  const firstThree = stats1.map( (stat, index) => {
-      console.log(stat)
+  const firstThree = stats1.map( (stat) => {
       return (
-        // <li key={stat[2]} className="grid-cell">
-          <DashboardProfileStatListItem stat={stat} key={index}/>
-        // </li>
+        <li key={stat[2]} className="grid-cell">
+          <DashboardProfileStatListItem stat={stat} />
+        </li>
       )
     }
   )
-  const lastThree = stats2.map( (stat, index) => {
+  const lastThree = stats2.map( (stat) => {
       return (
-        // <li key={stat[2]} className="grid-cell">
-          <DashboardProfileStatListItem stat={stat} key={index} />
-        // </li>
+        <li key={stat[2]} className="grid-cell">
+          <DashboardProfileStatListItem stat={stat} />
+        </li>
       )
     }
   )
