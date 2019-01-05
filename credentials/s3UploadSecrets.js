d952b01940c0bceff53f985e89b1ce7e1d9cf669
Seth <sethborne@gmail.com>
2019-01-04 16:53:35 -0800

    update react branch to create react app - deletions

--- credentials/s3UploadSecrets.js
+++
@@ -1,6 +0,0 @@
-import s3UploadSecrets from '../secrets/s3UploadSecrets.json'
-// import s3UploadSecrets from '../secrets/BETA_s3UploadSecrets.json'
-// console.log(s3UploadSecrets);
-
-export const s3UploadCreds = s3UploadSecrets
-// console.log('s3UploadCreds', s3UploadCreds);
--- src/reducers/reducer_currentUser.js
+++
@@ -1,8 +0,0 @@
-import Random from '../functions/random'
-
-const Users = require('!json-loader!../../data_returns/RecursiveThinkingDeveloperProfiles.json')
-
-export default function(state){
-  // return Users[Random.getRandomIndex(Users.length)]
-  return Users[2]
-}
