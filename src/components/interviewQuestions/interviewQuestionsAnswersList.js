ce12e33ff692467aa3d71f5e95fd29eb72ba21f1
Seth Borne <sethborne@gmail.com>
2019-02-14 10:23:57 -0800

    update interview question answers component

--- src/components/interviewQuestions/interviewQuestionsAnswersList.js
+++ src/components/interviewQuestions/interviewQuestionsAnswersList.js
@@ -5,14 +5,35 @@ import React from 'react'
 import UtilityMethods from '../../functions/utilityMethods'
 import DateMethods from '../../functions/dateMethods'
 
-// import DM from '../../standards/dictModel'
+import DM from '../../standards/dictModel'
+
+import { ROUTES_REACT } from '../../standards/routes'
 
 const interviewQuestionsAnswersList = ({...props}) => {
+  const {
+    lookupTableUsers,
+    intQuestionAnswers,
+    lookupTableInterviewQuestionsAnswers,
+    currentUser,
+    intQuestionId
+  } = props;
+  const {
+    intQuestionAnswer: {
+      Id,
+      description,
+      _createdByUser,
+      createdAt
+    }
+  } = DM;
+  const {
+    interviewquestionsanswers_edit,
+    interviewquestionsanswers_delete
+  } = ROUTES_REACT;
   // console.log(intQuestionAnswers)
-  let allAnswersArray = UtilityMethods.getArrayOfObjectsByKey(props.lookupTableInterviewQuestionsAnswers, props.intQuestionAnswers)
+  let allAnswersArray = UtilityMethods.getArrayOfObjectsByKey(lookupTableInterviewQuestionsAnswers, intQuestionAnswers)
   
   // console.log('allAnswers', allAnswersArray)
-  
+  // console.log('intQuestId', intQuestionId)
   if(allAnswersArray.length === 0){
     // then render a no answers message
     return (
@@ -22,37 +43,29 @@ const interviewQuestionsAnswersList = ({...props}) => {
     // we have messages    
     let allAnswers = allAnswersArray.map(answer => {
       // get date in custom format
-      let formattedDate = DateMethods.getFormattedDate(answer['createdAt']);
+      let formattedDate = DateMethods.getFormattedDate(answer[createdAt]);
 
       let answerWrittenByUser = ''
-      if(props.lookupTableUsers[answer['_createdByUser']]){
-        answerWrittenByUser = props.lookupTableUsers[answer['_createdByUser']].name
+      if(lookupTableUsers[answer[_createdByUser]]){
+        answerWrittenByUser = lookupTableUsers[answer[_createdByUser]].name
       }
 
-      let optionList = UtilityMethods.generateOptionsList(props.currentUser.userId, props.currentUser.admin, answer['_createdByUser'], 'fs14')
-      // console.log(optionList)
-      // this returns each li
+      let optionList = UtilityMethods.generateOptionsListAnswers(currentUser.userId, currentUser.admin, intQuestionId, interviewquestionsanswers_edit, interviewquestionsanswers_edit, interviewquestionsanswers_delete, answer[_createdByUser], answer[Id], 'fs14')
+
       return (
-        <li key={answer.Id} className="fc-answers">
-          {/* <div className="fc--disp-flex fc--fdir-row"> */}
-            <div className="grid grid--cols-2">
-              <div className="grid-cell">
-                {/* <h5 className="colorBlack fw900">{answerWrittenBy.name}</h5> */}
-                <h6 className="fs16 fw900 ls10 fcBlack">{answerWrittenByUser}</h6>
-              </div>
-              <div className="grid-cell">
-                <div className="fc--disp-flex fc--fdir-row fc--jCont-fe fc--aItem-ce">
-                  <div className="listOptions fc--disp-flex">
-                    <ul>
-                      {optionList}
-                    </ul>
-                  </div>
-                  <h6 className="fs16 fcGrey55 fw300 ml20">{formattedDate['upComingDateStringAmericanWithSlash']}</h6>
-                </div>
-              </div>
+        <li key={answer[Id]} className="fc-answers">
+          <div className="grid grid--cols-2">
+            <div className="grid-cell">
+              <h6 className="fs16 fw900 ls10 fcBlack">{answerWrittenByUser}</h6>
             </div>
-          {/* </div> */}
-          <p className="fs14 colorGray55 ls08 fw300">{answer['description']}</p>
+            <div className="grid-cell fc--disp-flex fc--jCont-fe fc--aItem-ce">
+              <ul className="listOptions fc--disp-flex">
+                {optionList}
+              </ul>
+              <h6 className="fs16 fcGrey55 fw300 ml20">{formattedDate['upComingDateStringAmericanWithSlash']}</h6>
+            </div>
+          </div>
+          <p className="fs14 colorGray55 ls08 fw300">{answer[description]}</p>
           <hr />
         </li>
       );
