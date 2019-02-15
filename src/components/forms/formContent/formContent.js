0602e1c66c2cbb883bc093416fa1dac06e77fb42
Seth Borne <sethborne@gmail.com>
2019-02-14 16:54:59 -0800

    moves formContent to form folder

---
+++ src/components/forms/formContent/formContent.js
@@ -0,0 +1,31 @@
+export const FORM_HEADING_LESSON_CREATE = {
+  heading: 'Create A Lesson',
+  buttonText: 'Submit Lesson'
+}
+
+export const FORM_HEADING_LESSON_EDIT = {
+  heading: 'Edit Lesson: ',
+  buttonText: 'Submit Editted Lesson'
+}
+
+export const FORM_HEADING_INTERVIEWQUESTION_CREATE = {
+  heading: 'Create An Interview Question',
+  buttonText: 'Submit Interview Question'
+}
+
+export const FORM_HEADING_INTERVIEWQUESTION_EDIT = {
+  heading: 'Edit Interview Question: ',
+  buttonText: 'Submit Editted Interview Question'
+}
+
+export const FORM_HEADING_INTERVIEWQUESTIONANSWER_CREATE = {
+  heading: 'Create An Answer To: ',
+  labelAnswer: 'Submit Your Answer',
+  buttonText: 'Submit Answer'
+}
+
+export const FORM_HEADING_INTERVIEWQUESTIONANSWER_EDIT = {
+  heading: 'Edit Answer To: ',
+  labelAnswer: 'Edit Your Answer',
+  buttonText: 'Submit Editted Answer'
+}
