7c8a775a06e9415d39950d0890c3e4afc7187bd4
Seth Borne <sethborne@gmail.com>
2019-02-19 12:30:00 -0800

    fixes form lessonDelete by removing form element

--- src/components/forms/form_lessonDelete.js
+++ src/components/forms/form_lessonDelete.js
@@ -60,7 +60,7 @@ class LessonDelete extends Component {
           <fieldset className="fc--disp-flex fc--fdir-col fc--aItem-ce">
             <h5 className="fw700 ls14 ttup fcGrey424041">Delete Lesson: {lessonById.title}</h5>
             <hr className="modalHR mt10" />
-            <form>
+            {/* <form> */}
               <div className="fc-fieldset">
                 <div className="fc-field fc--disp-flex fc--fdir-col fc--jCont-ce width100P">
                   <div className="fc-field-row-full fc--disp-flex fc--fdir-row mt10">
@@ -94,7 +94,7 @@ class LessonDelete extends Component {
                 >Delete Lesson</button>
               </div>
               {/* btn btnFillClrSchWarn btnOutlineClrSchUnavailable btnVoted fs16 fw500 ls12 ta-cent pdTB1p25LR2p5 */}
-            </form>
+            {/* </form> */}
           </fieldset>
         </article>
       )
