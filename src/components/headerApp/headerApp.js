9fb0092dcdf4036d53cfc72a8c870fbb36fd91a2
Seth Borne <sethborne@gmail.com>
2019-02-28 06:14:06 -0800

    cleanup header components

--- src/components/headerApp/headerApp.js
+++ src/components/headerApp/headerApp.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import { ROUTES_REACT } from '../../standards/routes';
 import { PATH_FOR_IMAGES } from '../../standards/publicPaths';
 
-// const headerApp = () => {
-  const headerApp = () => {
-    return (
-      <div className="grid grid--cols-2">
-        <div className="grid-cell fc--disp-flex fc-logo">
-          <div className="fc--disp-flex ">
-            <figure>
-              <img 
-                src={`${PATH_FOR_IMAGES}recursivelogo.png`}
-                alt="Recursive Thinking Logo"
-                />
-            </figure>
-          </div>
-        </div>
-        <div className="grid-cell fc--disp-flex fc-search">
-          <Link to={ROUTES_REACT.root}>
-            <button type="button" className="btn btnTemp btnPadTB1 btnPadLR1 fs16 ls12">Home</button>
-          </Link>
-          <Link to={ROUTES_REACT.dashboard}>
-            <button type="button" className="btn btnTemp btnPadTB1 btnPadLR1  fs16 ls12">Dash</button>
-          </Link>
-          <Link to={ROUTES_REACT.users_create}>
-            <button type="button" className="btn btnTemp btnPadTB1 btnPadLR1 fs16 ls12">SetupProfile</button>
-          </Link>
-          {/* <div className="fc--disp-flex search">
-            <div className="searchBar">
-              <input className="inputSearch" type="search" placeholder="Search" />
-            </div>
-          </div> */}
+const headerApp = () => {
+  return (
+    <div className="grid grid--cols-2">
+      <div className="grid-cell fc--disp-flex fc-logo">
+        <div className="fc--disp-flex ">
+          <figure>
+            <img 
+              src={`${PATH_FOR_IMAGES}recursivelogo.png`}
+              alt="Recursive Thinking Logo"
+              />
+          </figure>
         </div>
       </div>
-    )
+      <div className="grid-cell fc--disp-flex fc-search">
+        <Link to={ROUTES_REACT.root}>
+          <button type="button" className="btn btnTemp btnPadTB1 btnPadLR1 fs16 ls12">Home</button>
+        </Link>
+        <Link to={ROUTES_REACT.dashboard}>
+          <button type="button" className="btn btnTemp btnPadTB1 btnPadLR1  fs16 ls12">Dash</button>
+        </Link>
+        <Link to={ROUTES_REACT.users_create}>
+          <button type="button" className="btn btnTemp btnPadTB1 btnPadLR1 fs16 ls12">SetupProfile</button>
+        </Link>
+        {/* <div className="fc--disp-flex search">
+          <div className="searchBar">
+            <input className="inputSearch" type="search" placeholder="Search" />
+          </div>
+        </div> */}
+      </div>
+    </div>
+  )
 }
 
 export default headerApp
--- src/components/public/header/header.js
+++ src/components/public/header/header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
 
 import Modal from '../../common/modal/modal'
 import { SignUpFormEx, VerifyAccountFormEx, SignInFormEx } from '../../forms/forms_auth'
