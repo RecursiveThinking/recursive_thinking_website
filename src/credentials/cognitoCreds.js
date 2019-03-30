cf8f06d64c21aa23f36198b6f40f275a66fb9caf
Seth <sethborne@gmail.com>
2019-03-30 13:14:00 -0700

    update cognito creds

--- src/credentials/cognitoCreds.js
+++ src/credentials/cognitoCreds.js
@@ -2,4 +2,4 @@ import Creds from './secrets/cognitoSecrets.json'
 // console.log(creds);
 
 export const CREDENTIALS = Creds
-// console.log(credentials);
+// console.log('CREDENTIALS: ', CREDENTIALS);
