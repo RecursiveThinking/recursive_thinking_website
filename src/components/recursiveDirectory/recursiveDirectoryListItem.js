92c55dbc555c82509f82f5b193bd79d61caeae2f
Seth <sethborne@gmail.com>
2019-03-30 13:09:37 -0700

    cleanup classname

--- src/components/recursiveDirectory/recursiveDirectoryListItem.js
+++ src/components/recursiveDirectory/recursiveDirectoryListItem.js
@@ -26,7 +26,7 @@ const recursiveDirectoryListItem = ({user}) => {
       <Link to={`${users_view}/${user[userId]}`}>
         <h4 className="fs24 ls16 fw500 ta-cent mt25">{user[name]}</h4>
       </Link>
-      <hr className=" mtp125" />
+      <hr className="mtp125" />
       <h5 className="fs20 ls12 fw500 fcGrey424041 ta-cent mtp125">{user[title]}</h5>
       <h6 className="fs16 ls10 fw300 fcGrey424041 ta-cent">{concatLocation}</h6>
     </article>
