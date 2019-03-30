1a3f03880530275932a2b505e4cf313c45b83b54
Seth <sethborne@gmail.com>
2019-03-30 13:09:53 -0700

    update admin panel to function

--- src/containers/adminPanel/adminPanel.js
+++ src/containers/adminPanel/adminPanel.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux'
 
-import { fetchUsers, fetchLessons, fetchInterviewQuestions, fetchInterviewQuestionsAnswers} from '../../actions/index'
+import { fetchUsers, fetchLessons, fetchInterviewQuestions, fetchInterviewQuestionsAnswers} from '../../actions/index';
+import { FETCHING } from '../../actions/action_types';
 
+import DefaultLoadingPage from '../../components/defaults/loadingPage/loadingPage'
 import AdminPanelList from '../../components/adminPanel/adminPanelList'
+
+import ContentPageTitleBar from '../../components/common/contentPage/contentPageTitleBar'
+import { TITLE_BAR_ADMIN_PANEL } from '../../components/common/contentPage/contentPageTitleBarInfo';
 
 class AdminPanel extends Component {
   componentDidMount(){
@@ -14,21 +19,41 @@ class AdminPanel extends Component {
     this.props.fetchInterviewQuestionsAnswers();
   }
   render(){
-    // console.log('this.props', this.props)
-    
-    return(
-      <main>
-        <div className="content">
-          <AdminPanelList
-            allUsers={this.props.allUsers}
-            lookupTableUsers={this.props.lookupTableUsers}
-            allLessons={this.props.allLessons}
-            allInterviewQuestions={this.props.allInterviewQuestions}
-            allInterviewQuestionsAnswers={this.props.allInterviewQuestionsAnswers}
+    console.log('@ adminPanel Container - this.props: ', this.props)
+    const {
+      allUsers,
+      lookupTableUsers,
+      allLessons,
+      allInterviewQuestions,
+      allInterviewQuestionsAnswers
+    } = this.props;
+    if(allUsers === FETCHING || lookupTableUsers === FETCHING || allLessons === FETCHING || allInterviewQuestions === FETCHING || allInterviewQuestionsAnswers === FETCHING){
+      return (
+        <main>
+          <div className="content">
+            <DefaultLoadingPage />
+          </div>
+        </main>
+      )
+    } else {
+      return(
+        <main>
+          <ContentPageTitleBar 
+            content={TITLE_BAR_ADMIN_PANEL}
           />
-        </div>
-      </main>
-    )
+          <div className="content">
+            {/* <div>Howdy Partner</div> */}
+            <AdminPanelList
+              allUsers={this.props.allUsers}
+              lookupTableUsers={this.props.lookupTableUsers}
+              allLessons={this.props.allLessons}
+              allInterviewQuestions={this.props.allInterviewQuestions}
+              allInterviewQuestionsAnswers={this.props.allInterviewQuestionsAnswers}
+            />
+          </div>
+        </main>
+      )
+    }
   }
 }
 
@@ -42,7 +67,7 @@ function mapStateToProps(state){
     lookupTableInterviewQuestions: state.interviewQuestions.lookupTableInterviewQuestions,
     allInterviewQuestionsAnswers: state.interviewQuestionsAnswers.allInterviewQuestionsAnswers,
     lookupTableInterviewQuestionsAnswers: state.interviewQuestionsAnswers.lookupTableInterviewQuestionsAnswers,
-    currentUser: state.currentUser
+    currentUser: state.auth.currentUser
   }
 }
 
