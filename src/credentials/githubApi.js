6575b16cc57b10c48d6ef6c3733818db75a7f8fd
Seth <sethborne@gmail.com>
2019-03-30 13:14:24 -0700

    adds creds file for github and codepen validation

---
+++ src/credentials/githubApi.js
@@ -0,0 +1,11 @@
+import apiKey from './secrets/apiSecrets.json';
+// console.log('apiKey: ', apiKey);
+
+export const GITHUB_CLIENT_ID = apiKey.githubClientID;
+// console.log('GITHUB_CLIENT_ID: ', GITHUB_CLIENT_ID);
+
+export const GITHUB_CLIENT_SECRET = apiKey.githubClientSecret;
+// console.log('GITHUB_CLIENT_SECRET: ', GITHUB_CLIENT_SECRET);
+
+export const CODEPEN_API_URL = apiKey.codepenURL;
+// console.log('CODEPEN_API_URL: ', CODEPEN_API_URL);
