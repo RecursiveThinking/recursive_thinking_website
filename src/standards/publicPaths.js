68165850ee74085fcd3b08d39f2cbf05e6449c6c
Seth Borne <sethborne@gmail.com>
2019-04-13 11:50:36 -0700

    add s3 pathing for avatars

--- src/standards/publicPaths.js
+++ src/standards/publicPaths.js
@@ -1,2 +1,6 @@
+import { rootBucketName } from '../functions/s3Methods';
+
+export const PUBLIC_S3_URL = `https://s3-us-west-2.amazonaws.com/${rootBucketName}/`
+
 export const PATH_FOR_IMAGES = process.env.PUBLIC_URL + '/assets/images/'
 export const PATH_FOR_ANIMATIONS = process.env.PUBLIC_URL + '/assets/animations/'
